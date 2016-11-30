2695416bf20b3afa1db1b56388cf73dd4373fb7e
kamal ejaz <a2ninek@yahoo.com>
2016-11-30 21:34:33 +0500

    Fixes

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,25 +1,19 @@
 require('dotenv').config();
 
 var Sequelize = require('sequelize');
-if(!process.env.DATABASE_URL){
-  /*
+if(process.env.DATABASE_URL){
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  });
+} else {
   var sequelize = new Sequelize("AngularDemoApp", "root", "password", {
     dialect: "mysql",
     port: 3306
   });
-*/
-} else {
-  var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
-  var sequelize = new Sequelize(match[5], match[1], match[2], {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging: false,
-    dialectOptions: {
-      ssl: true
-    }})
-
 }
 sequelize
   .authenticate()
