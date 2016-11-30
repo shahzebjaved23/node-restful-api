1312315a1d449251f0f9e2c9587fdbf13854084c
kamal ejaz <a2ninek@yahoo.com>
2016-11-30 21:39:46 +0500

    Fixes

--- migrations/20161122055858-users-migration.js
+++ migrations/20161122055858-users-migration.js
@@ -3,7 +3,50 @@ var UserMeta = require("./../app/model/User")
 module.exports = {
   up: function (queryInterface, Sequelize) {
     queryInterface.createTable(
-      "users", UserMeta.attributes
+      "users", {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            is: /^[a-z0-9\_\-]+$/i,
+          }
+        },
+        email: {
+          type: Sequelize.STRING,
+          unique: true,
+          allowNull: false,
+          validate: {
+            isEmail: true
+          }
+        },
+        gender: {
+          type: Sequelize.STRING,
+        },
+        firstName: {
+          type: Sequelize.STRING,
+        },
+        lastName: {
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.STRING,
+        },
+        salt: {
+          type: Sequelize.STRING
+        }
+      }
     )
   },
   down: function (queryInterface, Sequelize) {
