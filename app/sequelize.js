caae9863149e5c87a3b12622dd58576f000d5e01
kamal ejaz <a2ninek@yahoo.com>
2016-11-30 20:31:45 +0500

    fixes

--- app/sequelize.js
+++ app/sequelize.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 var Sequelize = require('sequelize');
 if(process.env.NODE_ENV === "production"){
   var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
