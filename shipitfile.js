4a1a316c31d516728504edece15ec8975d44632e
kamal ejaz <a2ninek@yahoo.com>
2016-12-27 23:25:06 +0500

    shipit integration for deployment

---
+++ shipitfile.js
@@ -0,0 +1,20 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/home/ubuntu/www',
+      repositoryUrl: 'https://kamal-dev@gitlab.com/Shaful/shaful-api.git',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 4,
+      deleteOnRollback: false,
+      branch: 'development',
+      shallowClone: true
+    },
+    staging: {
+      servers: 'ubuntu@35.166.70.82'
+    }
+  });
+
+};
