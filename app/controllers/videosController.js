fff7d9ef4379cbe1499f73858a785225e5907777
shahzebjaved <shahzebjaved23@gmail.com>
2017-02-08 21:57:24 +0500

    added multipart support to videos

--- app/controllers/videosController.js
+++ app/controllers/videosController.js
@@ -1,6 +1,8 @@
 "use strict";
 var Model = require('../model/models.js'),
   S3Upload = require('../modules/S3Upload.js');
+var path = require('path');
+var fs = require('fs');
 
 module.exports.index = function (req, res) {
   Model.User.findById(req.user.id).then( (user) => {
@@ -16,28 +18,39 @@ module.exports.create = function(req, res) {
   
   let attrs = {};
   attrs["userId"] = req.user.id;
-  attrs["filePath"] = req.body.fileName;
-  attrs["fileData"] = req.body.fileData;
+  attrs["filePath"] = req.files.file.originalFilename;
+  
+  // return res.status(200).json({
+  //   fileName: req.body.fileName,
+  //   fileData: req.body.fileData,
+  //   video: req.files
+  // });
 
-  return res.status(200).json({
-    fileName: req.body.fileName,
-    fileData: req.body.fileData,
-    video: req.files
-  });
+  fs.readFile(req.files.file.path,function(error,data){
 
-  // Model.Video.create(attrs)
-  //   .then((video) => {
-  //     var data = req.body.fileData,
-  //       dirName = `public/uploads/videos/${video.id}/${video.filePath}`;
-  //     S3Upload.upload(dirName, data, function(error, data) {
-  //       return res.status(200).json({
-  //         video: video
-  //       });
-  //     });
-  //   }).catch( (error) => {
-  //     res.status(400).json({
-  //       error: error
-  //     });
-  //   });
+    Model.Video.create(attrs)
+    .then((video) => {
+      var dirName = `public/uploads/videos/${video.id}/${video.filePath}`;
+      S3Upload.upload(dirName, data, function(error, data) {
+        
+        // unlink the file in temp storage
+        fs.unlink(req.files.file.path, function (err) {
+          if (err) {
+              console.error(err);
+          }
+          console.log('Temp File Delete');
+        });
+
+        // return the response
+        return res.status(200).json({
+          video: video
+        });
+      });
+    }).catch( (error) => {
+      res.status(400).json({
+        error: error
+      });
+    });
+  }
 
 }
