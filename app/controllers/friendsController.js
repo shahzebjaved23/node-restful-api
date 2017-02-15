989bdb37861ab5a424b92d6dce3efcb3fc55bfa3
shahzebjaved <shahzebjaved23@gmail.com>
2017-02-15 18:07:17 +0500

    added the user info in requests and fixed the get friends method

--- app/controllers/friendsController.js
+++ app/controllers/friendsController.js
@@ -6,26 +6,44 @@ var Model = require('../model/models.js'),
 var sequelize = require("../sequelize.js");
 
 module.exports.index = function(req, res) {
-  Model.connection.query('SELECT *, EXISTS(SELECT * from users_friends as ufo where ufo.userId = :userId AND ufo.friendId = users.id) as connected FROM users where users.id != :userId',
-    {replacements: { userId: req.user.id }, type: Sequelize.QueryTypes.SELECT })
-    .then((data) => {
-      var friends = _.map(data, (hsh) => {
-        var fileName = "public/uploads/users/" + hsh.id + "/" + hsh.image;
-        fileName = `https://${process.env.SHAFUL_S3_BUCKET}.s3.amazonaws.com/${fileName}`;
-        return {
-          fullName: hsh.firstName + hsh.lastName,
-          id: hsh.id,
-          profile_picture: fileName,
-          connected: hsh.connected
-        };
-      });
-      return res.status(200).json({
-        friends: friends
+
+  // get the user id
+  var userId = req.user.id;
+
+  // select the user ids that are friends of the above user id
+  sequelize.query("SELECT users.id FROM `users` WHERE users.id IN (SELECT friendId FROM users_friends WHERE userId = "+userId+") OR users.id IN (SELECT userId FROM users_friends WHERE friendId = "+userId+")",{ type: sequelize.QueryTypes.SELECT}).then((friends)=>{
+
+      // map them in an array friends_ids
+      var friends_ids = [];
+      friends.map(function(friend){
+        friends_ids.push(friend.id);
       })
-    }).catch(function(error) {
-      return res.status(400).json({
-        error: error
-      });
+
+      if (friends_ids.length > 0){
+        // get the posts and photos of the friends_ids
+        Model.User.findAll({
+          where:{
+            id:{
+              $in: friends_ids  
+            } 
+          }
+        })
+        .then( (friends)=>{
+            return res.status(200).json({
+              friends:friends
+            });
+          } ).catch((error)=>{
+            return res.status(400).json({
+              error: error
+            });
+          }); 
+      }else{
+        return res.status(200).json({
+          message: "you have no friends"
+        })
+      }
+
+      
     });
 }
 
@@ -71,10 +89,25 @@ in the users_friends table,
 module.exports.getFriendRequests = function(req,res){
   var userId = req.user.id;
   
-  sequelize.query("select * from users_friends where friendId="+userId+" and status='pending'", {type: Sequelize.QueryTypes.SELECT }).then(function(data){
-    return res.status(200).json({
-      friendRequests: data
+  sequelize.query("(select userId from users_friends where friendId="+userId+" and status='pending')", {type: Sequelize.QueryTypes.SELECT }).then(function(data){
+    
+    var request_ids = data.map(function(data){
+      return data.userId
     })
+    
+    Model.User.findAll({
+      where:{
+        id:{
+          $in:request_ids
+        }
+      }
+    }).then(function(data){
+      return res.status(200).json({
+        RequestsBy: data
+      })  
+    })
+
+    
   });
 }
 
